1322279ac2789829221f61bd2c7f47897550c0d8
fusj1 <38058345+fusj1@users.noreply.github.com>
2021-11-07 13:27:42 +0800

    informer ways

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module k8sdev
 go 1.16
 
 require (
+	k8s.io/api v0.22.3
 	k8s.io/apimachinery v0.22.3
 	k8s.io/client-go v0.22.3
 )
--- main.go
+++ main.go
@@ -1,25 +1,19 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
-	"os"
 	"path"
+	"time"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 )
-
-func HomeDir() string {
-	if homedir := os.Getenv("HOME"); homedir != "" {
-		return homedir
-	} else {
-		return os.Getenv("USERPROFILE")
-	}
-}
 
 func main() {
 	var config *rest.Config
@@ -41,12 +35,41 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	deployments, err := clientset.AppsV1().Deployments("default").List(context.TODO(), metav1.ListOptions{})
+	// 初始化一个工厂函数
+	informerfactory := informers.NewSharedInformerFactory(clientset, time.Second*30)
+
+	// 初始化一个deploments的 informer
+	deployinformer := informerfactory.Apps().V1().Deployments()
+
+	// 创建 Informer（相当于注册到工厂中去，这样下面启动的时候就会去 List & Watch 对应的资源）
+	informer := deployinformer.Informer()
+	// 启动一个deploylistener
+	deploylistener := deployinformer.Lister()
+	// 注册事件处理函数
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			deploy := obj.(*appsv1.Deployment)
+			fmt.Println("Add deployments:", deploy.Name)
+		},
+		UpdateFunc: func(OldObj, NewObj interface{}) {
+			olddeploy := OldObj.(*appsv1.Deployment)
+			newdeploy := NewObj.(*appsv1.Deployment)
+			fmt.Printf("%s->%s", olddeploy.Name, newdeploy.Name)
+		},
+		DeleteFunc: func(obj interface{}) {
+			deploy := obj.(*appsv1.Deployment)
+			fmt.Println("Delete deployments:", deploy.Name)
+		},
+	})
+	stopper := make(chan struct{})
+	// 启动工厂函数监听
+	informerfactory.Start(stopper)
+	defer close(stopper)
+	deploys, err := deploylistener.Deployments("default").List(labels.Everything())
 	if err != nil {
 		panic(err.Error())
 	}
-	for idx, deploy := range deployments.Items {
-		fmt.Printf("%d->%s", idx+1, deploy.Name)
+	for idx, deploy := range deploys {
+		fmt.Printf("%d -->%s", idx+1, deploy.Name)
 	}
-
 }
